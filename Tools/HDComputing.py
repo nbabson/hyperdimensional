8f5bac961672e3b71141f2f3d855427099841599
Ruchit Kank <rkank@pdx.edu>
2020-08-27 13:55:32 -0700

    Cleaned record-encoding

--- Tools/HDComputing.py
+++ Tools/HDComputing.py
@@ -6,6 +6,7 @@ from scipy.spatial.distance import hamming, cosine
 from functools import wraps
 from typing import Type, Tuple, Union
 from mypy_extensions import TypedDict
+from abc import ABC, abstractmethod
 
 random.seed()
 
@@ -14,9 +15,10 @@ random.seed()
 # TODO: Majority-rule additions
 # TODO: n-gram
 # TODO: Overload iterator for Vector
-# TODO: Check permutation_indices being shared among different sub-classes
+# TODO: Add empty value init for all
 
 # VERIFY: Verify permutation
+
 
 def add_method(cls):
 	def decorator(func):
@@ -30,10 +32,13 @@ def add_method(cls):
 	return decorator
 
 
-class Vector:
-	# Static dictionary to maintain common permutation indices across objects
-	permutation_indices = {}
+class Vector(ABC):
+	def __init_subclass__(cls, **kwargs):
+		if not hasattr(cls, 'permutation_indices'):
+			raise TypeError("Can't instantiate class " + cls.__name__ + " with abstract variable permutation_indices")
+		return super().__init_subclass__(**kwargs)
 
+	@abstractmethod
 	def __init__(self, dim: Union[Tuple[int, int], int], value: np.ndarray):
 		"""
 		:param dim: dimension or tuple of (number of vectors, dimension)
@@ -46,26 +51,31 @@ class Vector:
 		self.value = value
 
 		# Add permutation indices to static dictionary for first occurrence of dimension
-		if self.dim not in type(self).permutation_indices:
-			type(self).permutation_indices[self.dim] = np.random.permutation(self.dim)
+		if self.dim not in self.__class__.permutation_indices:
+			self.__class__.permutation_indices[self.dim] = np.random.permutation(self.dim)
 
+	@abstractmethod
 	def __add__(self, other: Vector) -> Vector:
 		"""
 		Bundling operator
 		"""
-		raise NotImplementedError("Bundling operation '+' must be implemented")
+		# raise NotImplementedError("Bundling operation '+' must be implemented")
+		pass
 
+	@abstractmethod
 	def __mul__(self, other: Vector) -> Vector:
 		"""
 		Binding operator
 		"""
-		raise NotImplementedError("Binding operation '*' must be implemented")
+		# raise NotImplementedError("Binding operation '*' must be implemented")
+		pass
 
 	def __invert__(self):
 		"""
 		Permutation operator
 		"""
-		return type(self)(dim=self.dim, value=self.value[type(self).permutation_indices[self.dim]])
+		permuted_value = self.value[self.__class__.permutation_indices[self.dim]]
+		return self.__class__(dim=self.dim, value=permuted_value)
 
 	def __or__(self, other: Vector) -> Vector:
 		"""
@@ -91,11 +101,28 @@ class Vector:
 		"""
 		return type(self)(self.dim, self.value[item])
 
+	def __setitem__(self, key, value) -> None:
+		"""
+		Set value in multi-valued hypervectors
+		"""
+		if type(value) is np.ndarray:
+			self.value[key] = value
+		elif isinstance(value, Vector):
+			self.value[key] = value.value
+		else:
+			raise TypeError('Value must be an np.ndarray or object of a Vector sub-class')
+
 
 class BSCVector(Vector):
-	def __init__(self, dim: Union[Tuple[int, int], int], value: np.ndarray = None):
+	# Static dictionary to maintain common permutation indices across objects
+	permutation_indices = {}
+
+	def __init__(self, dim: Union[Tuple[int, int], int], value: np.ndarray = None, empty: bool = False):
 		if value is None:
-			value = np.random.randint(2, size=dim)
+			if empty:
+				value = np.zeros(shape=dim, dtype=np.int)
+			else:
+				value = np.random.randint(2, size=dim)
 		super().__init__(dim, value)
 
 	def __add__(self, x: BSCVector) -> BSCVector:
@@ -122,6 +149,9 @@ class BSCVector(Vector):
 
 
 class BipolarVector(Vector):
+	# Static dictionary to maintain common permutation indices across objects
+	permutation_indices = {}
+
 	def __init__(self, dim: Union[Tuple[int, int], int], value: np.ndarray = None):
 		if value is None:
 			value = np.random.choice([-1.0, 1.0], size=dim)
@@ -151,6 +181,9 @@ class BipolarVector(Vector):
 
 
 class BSDVector(Vector):
+	# Static dictionary to maintain common permutation indices across objects
+	permutation_indices = {}
+
 	def __init__(self, dim: Union[Tuple[int, int], int], value: np.ndarray = None):
 		# TODO make probability as a param
 		if value is None:
@@ -228,7 +261,7 @@ def record_encode(dim: int, rep: Type[Vector], enc: RecordEncodingDict, features
 	"""
 	# VERIFY: Infer N from dimensions of features
 	# Create empty vector of given representation
-	S = rep(dim=dim, value=np.zeros(dim))
+	S = rep(dim=dim, empty=True)
 
 	if id_vectors is None:
 		# id_vectors = np.random.randint(2, size=(enc['N'], dim))
@@ -237,7 +270,7 @@ def record_encode(dim: int, rep: Type[Vector], enc: RecordEncodingDict, features
 
 		num_flip = int(dim / (enc['M'] - 1))
 		l_vectors = np.zeros(shape=(enc['M'], dim), dtype=np.int)
-		l_vectors[0] = np.random.randint(2, size=dim, dtype=int)
+		l_vectors[0] = np.random.randint(2, size=dim, dtype=np.int)
 
 		# TODO: Fix flipping for reps other than {0,1} values
 		for index in range(1, enc['M']):
@@ -250,19 +283,25 @@ def record_encode(dim: int, rep: Type[Vector], enc: RecordEncodingDict, features
 	bins = np.linspace(enc['range'][0], enc['range'][1], enc['M'] + 1)
 	level_dict = {(x, y): l for x, y, l in zip(bins, bins[1:], level_vectors)}
 
-	encoded = np.empty((enc['N'], dim))
+	encoded = rep(dim=dim, value=np.zeros((enc['N'], dim)))
 	for num, feature in enumerate(features):
 		# encoded[num] = [self.mul_bsc(level_dict[(low, high)], id_vectors[num]) for (low, high) in level_dict if (feature >= low) and (feature <= high)][0]
-		for (low, high) in level_dict:
-			if (feature >= low) and (feature <= high):
-				v1 = level_dict[(low, high)]
-				v2 = id_vectors[num]
-				val = v1 * v2
-				encoded[num] = val.value
+		# VERIFY: Biased towards higher bins, randomly choose?
+		for (low, high), level_vector in level_dict.items():
+			if low <= feature < high:
+				# v1 = level_vector
+				# v2 = id_vectors[num]
+				# val = v1 * v2
+				# encoded[num] = level_vector * id_vectors[num]
+				S = S + (level_vector * id_vectors[num])
+
+				# print('Encoded ID vector ' + str(num) + ' at level (' + str(low) + ', ' + str(high) + ')')
 				break
 
-	for vector in encoded:
-		S = S + rep(dim=dim, value=vector)
+	# for vector in encoded:
+	# 	S = S + vector
+
+	S.value = S.value.astype(dtype=np.int)
 
 	return id_vectors, level_vectors, S
 
@@ -299,23 +338,31 @@ class Hyperspace:
 
 		:param name: Name for hypervector, randomly generated if empty
 		:param features: ndarray of features for record-based or n-gram encoding
+		:param replace: replace vector with existing name, else adds to it
 		:return: Vector subclass defined in representation
 		"""
+		if name is None:
+			name = self.__random_name()
+
 		# TODO: Add option to specify number of vectors generated, return as list
 		if self.enc is not None and 'record' in self.enc:
 			if features is None:
 				raise ValueError('Record-based encoded vectors must supply features to be added')
 			elif self.id_vectors is None:
-				self.id_vectors, self.level_vectors, _ = record_encode(self.dim, self.rep, self.enc['record'], features)
+				self.id_vectors, self.level_vectors, v = record_encode(self.dim, self.rep, self.enc['record'], features)
 			else:
-				_, _, _ = record_encode(self.dim, self.rep, self.enc['record'], features)
+				_, _, v = record_encode(self.dim, self.rep, self.enc['record'], features)
 
-		if name is None:
-			name = self.__random_name()
+			# Build class hypervectors
+			if name not in self.vectors:
+				self.vectors[name] = v
+			else:
+				self.vectors[name] = self.vectors[name] + v
 
-		v = self.rep(dim=self.dim)
+		else:
+			v = self.rep(dim=self.dim)
+			self.vectors[name] = v
 
-		self.vectors[name] = v
 		return v
 
 	def insert(self, v: Type[Vector], name: str = None) -> None:
